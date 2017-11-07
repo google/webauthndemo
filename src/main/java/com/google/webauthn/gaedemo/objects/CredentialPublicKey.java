a80df3e71d7d2163f52b1339754ec0df133efe5d
Casey Piper <piperc@google.com>
2017-11-07 14:52:01 -0800

    Add COSE key format support

--- src/main/java/com/google/webauthn/gaedemo/objects/CredentialPublicKey.java
+++ src/main/java/com/google/webauthn/gaedemo/objects/CredentialPublicKey.java
@@ -19,12 +19,23 @@ import co.nstant.in.cbor.CborException;
 import co.nstant.in.cbor.model.ByteString;
 import co.nstant.in.cbor.model.DataItem;
 import co.nstant.in.cbor.model.Map;
-import co.nstant.in.cbor.model.UnicodeString;
+import co.nstant.in.cbor.model.NegativeInteger;
+import co.nstant.in.cbor.model.UnsignedInteger;
+import java.security.InvalidParameterException;
 import java.util.List;
 
 public abstract class CredentialPublicKey {
   Algorithm alg;
+  int kty;
   byte[] cborEncodedKey;
+  static final int CRV_LABEL = -1;
+  static final int X_LABEL = -2;
+  static final int Y_LABEL = -3;
+  static final int N_LABEL = -1;
+  static final int E_LABEL = -2;
+  static final int KTY_LABEL = 1;
+  static final int ALG_LABEL = 3;
+
 
   /**
    * @return Cbor encoded byte array
@@ -35,6 +46,7 @@ public abstract class CredentialPublicKey {
   /**
    * @return human-readable hex string of the key
    */
+  @Override
   public abstract String toString();
 
   /**
@@ -43,52 +55,79 @@ public abstract class CredentialPublicKey {
    * @throws CborException
    */
   public static CredentialPublicKey decode(byte[] cbor) throws CborException {
-    CredentialPublicKey cpk = null;
-    byte[] cborEncodedKey = cbor;
     List<DataItem> dataItems = CborDecoder.decode(cbor);
     if (dataItems.size() < 1 || !(dataItems.get(0) instanceof Map)) {
       return null;
     }
 
     Map map = (Map) dataItems.get(0);
-    for (DataItem key : map.getKeys()) {
-      UnicodeString keyString = (UnicodeString) key;
-      if (keyString.getString().equals("alg")) {
-        String algorithm = ((UnicodeString) map.get(keyString)).getString();
-        Algorithm alg = null;
-        try {
-          alg = Algorithm.decode(algorithm);
-        } catch (IllegalArgumentException e) {
-          break;
+    // If there are 4 keys in the map, the key should be RSA. If there are 5, then it is ECC.
+    if (map.getKeys().size() == 4) {
+      RsaKey rsaKey = new RsaKey();
+
+      for (DataItem d : map.getKeys()) {
+        int tmp = 0;
+        if (d instanceof NegativeInteger) {
+          tmp = ((NegativeInteger) d).getValue().intValue();
+        } else if (d instanceof UnsignedInteger) {
+          tmp = ((UnsignedInteger) d).getValue().intValue();
         }
-        if (Algorithm.isEccAlgorithm(alg)) {
-          EccKey ecc = new EccKey();
-          ecc.alg = alg;
-          ecc.cborEncodedKey = cborEncodedKey;
-          for (DataItem d : map.getKeys()) {
-            if (((UnicodeString) d).getString().equals("x")) {
-              ecc.x = ((ByteString) map.get(d)).getBytes();
-            } else if (((UnicodeString) d).getString().equals("y")) {
-              ecc.y = ((ByteString) map.get(d)).getBytes();
-            }
-          }
-          cpk = ecc;
-          break;
-        } else if (Algorithm.isRsaAlgorithm(alg)) {
-          RsaKey rsa = new RsaKey();
-          rsa.alg = alg;
-          for (DataItem d : map.getKeys()) {
-            if (((UnicodeString) d).getString().equals("n")) {
-              rsa.n = ((ByteString) map.get(d)).getBytes();
-            } else if (((UnicodeString) d).getString().equals("e")) {
-              rsa.e = ((ByteString) map.get(d)).getBytes();
-            }
-          }
-          cpk = rsa;
-          break;
+
+        switch (tmp) {
+          case N_LABEL:
+            rsaKey.n = ((ByteString) map.get(d)).getBytes();
+            break;
+          case E_LABEL:
+            rsaKey.e = ((ByteString) map.get(d)).getBytes();
+            break;
+          case KTY_LABEL:
+            rsaKey.kty = ((UnsignedInteger) map.get(d)).getValue().intValue();
+            break;
+          case ALG_LABEL:
+            rsaKey.alg =
+                Algorithm.decode(((NegativeInteger) map.get(d)).getValue().intValue());
+            if (!Algorithm.isRsaAlgorithm(rsaKey.alg))
+              throw new InvalidParameterException("Unsupported RSA algorithm");
+            break;
         }
       }
+      return rsaKey;
+
+    } else if (map.getKeys().size() == 5) {
+      EccKey eccKey = new EccKey();
+
+      for (DataItem d : map.getKeys()) {
+        int tmp = 0;
+        if (d instanceof NegativeInteger) {
+          tmp = ((NegativeInteger) d).getValue().intValue();
+        } else if (d instanceof UnsignedInteger) {
+          tmp = ((UnsignedInteger) d).getValue().intValue();
+        }
+
+        switch (tmp) {
+          case CRV_LABEL:
+            eccKey.crv = ((UnsignedInteger) map.get(d)).getValue().intValue();
+            break;
+          case X_LABEL:
+            eccKey.x = ((ByteString) map.get(d)).getBytes();
+            break;
+          case Y_LABEL:
+            eccKey.y = ((ByteString) map.get(d)).getBytes();
+            break;
+          case KTY_LABEL:
+            eccKey.kty = ((UnsignedInteger) map.get(d)).getValue().intValue();
+            break;
+          case ALG_LABEL:
+            eccKey.alg =
+                Algorithm.decode(((NegativeInteger) map.get(d)).getValue().intValue());
+            if (!Algorithm.isEccAlgorithm(eccKey.alg))
+              throw new InvalidParameterException("Unsupported ECC algorithm");
+            break;
+        }
+      }
+      return eccKey;
     }
-    return cpk;
+
+    throw new InvalidParameterException("Unsupported COSE public key sent");
   }
 }
