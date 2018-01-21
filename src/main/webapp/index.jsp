6a3e7cd7ab035f07ccee80865ddf8dfa3ef71586
Eiji Kitamura <agektmr@google.com>
2018-01-21 15:15:46 +0900

    More complete polymer implementation

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,4 @@
+<%@ page language="java" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,7 +7,21 @@
 <script src="bower_components/webcomponentsjs/webcomponents-loader.js"></script>
 <link rel="import" href="bower_components/polymer/polymer.html">
 <link rel="import" href="../bower_components/neon-animation/web-animations.html">
-<link rel="import" href="components/wad-app.html">
+<link rel="import" href="../bower_components/polymer/polymer-element.html">
+<link rel="import" href="../bower_components/app-layout/app-layout.html">
+<link rel="import" href="../bower_components/iron-list/iron-list.html">
+<link rel="import" href="../bower_components/paper-button/paper-button.html">
+<link rel="import" href="../bower_components/paper-toggle-button/paper-toggle-button.html">
+<link rel="import" href="../bower_components/iron-icon/iron-icon.html">
+<link rel="import" href="../bower_components/iron-icons/iron-icons.html">
+<link rel="import" href="../bower_components/paper-card/paper-card.html">
+<link rel="import" href="../bower_components/paper-dropdown-menu/paper-dropdown-menu.html">
+<link rel="import" href="../bower_components/paper-listbox/paper-listbox.html">
+<link rel="import" href="../bower_components/paper-item/paper-item.html">
+<link rel="import" href="../bower_components/paper-toast/paper-toast.html">
+<link rel="import" href="../bower_components/paper-progress/paper-progress.html">
+
+<!-- <link rel="import" href="components/wad-app.html"> -->
 <style>
   body {
     margin: 0;
@@ -15,10 +30,156 @@
     min-height: 100vh;
     background-color: #eeeeee;
   }
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  app-header {
+    background-color: rgb(0,150,136);
+    color: white;
+  }
+  paper-button {
+    font-size: 14px;
+  }
+  app-toolbar paper-button {
+    background-color: rgb(255,64,129);
+  }
+  .logout {
+    background-color: transparent;
+  }
+  .side-padding {
+    padding: 0 16px;
+  }
+  .card-actions {
+    text-align: right;
+  }
+  paper-progress {
+    width: 100%;
+  }
+  #options {
+    padding: 16px;
+    background-color: white;
+  }
+  #advanced > * {
+    margin-right: 16px;
+  }
+  #advanced paper-toggle-button {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  .subtitle-text {
+    font-size: 16px;
+    font-weight: bold;
+    color: rgba(0,0,0,.54);
+  }
+  .supporting-text {
+    padding-left: .2cm;
+    font-size: 14px;
+    font-style: italic;
+    overflow-wrap: break-word;
+    white-space: pre-wrap;
+    color: rgba(0,0,0,.54);
+  }
 </style>
 <title>WebAuthN Demo</title>
 </head>
 <body>
-  <wad-app></wad-app>
+  <dom-bind>
+    <template>
+      <custom-style>
+        <style is="custom-style">
+          paper-card {
+            margin: 30px;
+            max-width: 400px;
+            --paper-card-header: {
+              border-bottom: 1px solid #e8e8e8;
+            }
+            --paper-card-header-text: {
+              font-size: 18px;
+            }
+          }
+        </style>
+      </custom-style>
+      <app-header-layout>
+        <app-header slot="header" fixed shadow>
+          <app-toolbar>
+            <div main-title>WebAuthN Demo - ${nickname}</div>
+            <paper-button id="add" raised>Register New Credential</paper-button>
+            <paper-button id="auth" raised>Authenticate</paper-button>
+            <paper-button class="logout">
+              <a href="${logoutUrl}">Logout</a>
+            </paper-button>
+          </app-toolbar>
+          <section id="options">
+            <paper-toggle-button checked="{{advanced}}">Advanced Options</paper-toggle-button>
+            <template is="dom-if" if="[[advanced]]">
+              <div id="advanced">
+                <paper-toggle-button checked="{{excludeCredentials}}">Prevent Reregistration</paper-toggle-button>
+                <paper-dropdown-menu label="Attachment type">
+                  <paper-listbox
+                    attr-for-selected="item-name"
+                    selected="{{authenticatorAttachment}}"
+                    slot="dropdown-content">
+                    <paper-item item-name="none">N/A</paper-item>
+                    <paper-item item-name="platform">Platform</paper-item>
+                    <paper-item item-name="cross-platform">Cross-Platform</paper-item>
+                  </paper-listbox>
+                </paper-dropdown-menu>
+                <paper-dropdown-menu label="Conveyance Preference">
+                  <paper-listbox
+                    attr-for-selected="item-name"
+                    selected="{{attestationConveyancePreference}}"
+                    slot="dropdown-content">
+                    <paper-item item-name="NA">N/A</paper-item>
+                    <paper-item item-name="none">None</paper-item>
+                    <paper-item item-name="indirect">Indirect</paper-item>
+                    <paper-item item-name="direct">Direct</paper-item>
+                  </paper-listbox>
+                </paper-dropdown-menu>
+                <paper-toggle-button checked="{{requireResidentKey}}">Require resident key</paper-toggle-button>
+                <paper-dropdown-menu label="User Verification">
+                  <paper-listbox
+                    attr-for-selected="item-name"
+                    selected="{{userVerification}}"
+                    slot="dropdown-content">
+                    <paper-item item-name="none">None</paper-item>
+                    <paper-item item-name="required">Required</paper-item>
+                    <paper-item item-name="preferred">Preferred</paper-item>
+                    <paper-item item-name="discouraged">Discouraged</paper-item>
+                  </paper-listbox>
+                </paper-dropdown-menu>
+              </div>
+            </template>
+          </section>
+        </app-header>
+        <template is="dom-if" if="{{active}}">
+          <section id="active">
+            <paper-progress indeterminate class="blue"></paper-progress>
+            <h3 class="side-padding">Waiting for user touch</h3>
+          </section>
+        </template>
+        <section id="credentials">
+          <template is="dom-repeat" items="[[credentials]]">
+            <paper-card elevation="1" id="[[item.handle]]" heading="[[item.name]]">
+              <div class="card-content">
+                <div class="supporting-text">[[item.date]]</div>
+                <div class="subtitle-text">Public Key</div>
+                <div class="supporting-text">[[item.publicKey]]</div>
+                <div class="subtitle-text">Key Handle</div>
+                <div class="supporting-text">[[item.handle]]</div>
+              </div>
+              <div class="card-actions">
+                <paper-button raised id="[[item.id]]">
+                  <iron-icon icon="delete"></iron-icon>Delete
+                </paper-button>
+              </div>
+            </paper-card>
+          </template>
+        </section>
+      </app-header-layout>
+      <paper-toast id="toast" duration="5000"></paper-toast>
+    </template>
+  </dom-bind>
+  <script src="js/webauthn.js"></script>
 </body>
 </html>
--- src/main/webapp/js/webauthn.js
+++ src/main/webapp/js/webauthn.js
@@ -15,303 +15,289 @@
  *
  */
 
-const $ = query => {
-  return document.querySelector(query);
-};
+class WadApp {
+  constructor() {
+    this.app = document.querySelector('dom-bind');
+    this.app.active = false;
+    this.app.advanced = false;
+    this.app.excludeCredentials = false;
+    this.app.authenticatorAttachment = 'none';
+    this.app.attestationConveyancePreference = 'NA';
+    this.app.requireResidentKey = false;
+    this.app.userVerification = 'none';
 
-const show = query => {
-  $(query).style.display = 'block';
-};
+    this.toast = document.querySelector('#toast');
 
-const hide = query => {
-  $(query).style.display = 'none';
-};
-
-const isChecked = query => {
-  return $(query).checked;
-};
-
-const onClick = (query, func) => {
-  $(query).addEventListener('click', func);
-};
-
-const post = (url, obj) => {
-  let headers = new Headers({
-    'Content-Type': 'application/x-www-form-urlencoded'
-  });
-  let body = new URLSearchParams();
-  for (let key in obj) {
-    body.append(key, obj[key]);
-  }
-  return fetch(url, {
-    method: 'POST',
-    headers: headers,
-    credentials: 'include',
-    body: body
-  }).then(response => {
-    if (response.status === 200) {
-      return response.json();
-    } else {
-      throw response.statusText;
-    }
-  });
-};
-
-// function fetchAllCredentials() {
-//   post('/RegisteredKeys').then(tokens => {
-//     credentials.innerHTML='';
-//     for (let i in tokens) {
-//       let token = tokens[i];
-//       credentials.innerHTML += `<div id="credential${i}">${token.id}: ${token.handle}</div>`;
-//     }
-//   });
-// }
-
-document.addEventListener('DOMContentLoaded', () => {
-  let hiddens = document.querySelectorAll('.hidden');
-  for (let hidden of hiddens) {
-    hidden.style.display = 'none';
-    hidden.classList.remove('hidden');
-  }
-});
-
-window.addEventListener('load', () => {
-  onClick('#credential-button', addCredential);
-  onClick('#authenticate-button', getAssertion);
-  onClick('#switch-advanced', () => {
-    if (isChecked('#switch-advanced')) {
-      show('#advanced');
-    } else {
-      hide('#advanced');
-    }
-  });
-  fetchCredentials();
-});
-
-function credentialListConversion(list) {
-  return list.map(item => {
-    return {
-      type: item.type,
-      id: Uint8Array.from(atob(item.id), c => c.charCodeAt(0)),
-      transports: list.transports || undefined
-    };
-  });
-}
-
-function finishAddCredential(publicKeyCredential, sessionId) {
-  post('/FinishMakeCredential', {
-    data: JSON.stringify(publicKeyCredential),
-    session: sessionId }
-  ).then(parameters => {
-    console.log(parameters);
-    if ('success' in parameters && 'message' in parameters) {
-      addSuccessMsg(parameters.message);
-      fetchCredentials();
-    }
-    // TODO Validate response and display success/error message
-  });
-}
-
-function addCredential() {
-  removeMsgs();
-  show('#active');
-  var advancedOptions = {};
-  if (isChecked('#switch-advanced')) {
-    if (isChecked('#switch-rk')) {
-      advancedOptions.requireResidentKey = isChecked('#switch-rk');
-    }
-    if (isChecked('#switch-rr')) {
-      advancedOptions.excludeCredentials = isChecked('#switch-rk');
-    }
-    if ($('#userVerification').value != "none") {
-      advancedOptions.userVerification = $('#userVerification').value;
-    }
-    if ($('#attachment').value != "none") {
-      advancedOptions.authenticatorAttachment = $('#attachment').value;
-    }
-    if ($('#conveyance').value != "NA") {
-      advancedOptions.attestationConveyancePreference = $('#conveyance').value;
-    }
-  }
-
-  let options = {};
-
-  post('/BeginMakeCredential', {
-    advanced: isChecked('#switch-advanced'),
-    advancedOptions: JSON.stringify(advancedOptions) }
-  ).then(_options => {
-    options = _options;
-    let makeCredentialOptions = {};
-    makeCredentialOptions.rp = options.rp;
-    makeCredentialOptions.user = options.user;
-    makeCredentialOptions.user.id = new TextEncoder().encode(options.user.id);
-    makeCredentialOptions.challenge = Uint8Array.from(atob(options.challenge), c => c.charCodeAt(0));
-    makeCredentialOptions.pubKeyCredParams = options.pubKeyCredParams;
-
-    // Optional parameters
-    if ('timeout' in options) {
-      makeCredentialOptions.timeout = options.timeout;
-    }
-    if ('excludeCredentials' in options) {
-      makeCredentialOptions.excludeCredentials = credentialListConversion(options.excludeCredentials);
-    }
-    if ('authenticatorSelection' in options) {
-      makeCredentialOptions.authenticatorSelection = options.authenticatorSelection;
-    }
-    if ('attestation' in options) {
-      makeCredentialOptions.attestation = options.attestation;
-    }
-    if ('extensions' in options) {
-      makeCredentialOptions.extensions = options.extensions;
-    }
-
-    let createParams = {};
-    createParams.publicKey = makeCredentialOptions;
-
-    console.log(makeCredentialOptions);
-
-    // Check to see if the browser supports credential creation
-    if (typeof navigator.credentials.create !== "function") {
-      throw "Browser does not support credential creation";
-    }
-
-    return navigator.credentials.create({
-      "publicKey": makeCredentialOptions
+    document.querySelector('#add').addEventListener('click', () => {
+      this.addCredential();
     });
-  }).then(attestation => {
-    hide('#active');
-    let publicKeyCredential = {};
-    if ('id' in attestation) {
-      publicKeyCredential.id = attestation.id;
-    }
-    if ('type' in attestation) {
-      publicKeyCredential.type = attestation.type;
-    }
-    if ('rawId' in attestation) {
-      publicKeyCredential.rawId = btoa(
-        new Uint8Array(attestation.rawId).reduce((s, byte) =>
-        s + String.fromCharCode(byte), ''));
-      publicKeyCredential.rawId = attestation.rawId;
-    }
-    if ('response' in attestation) {
-      let response = {};
-      response.clientDataJSON = btoa(
-        new Uint8Array(attestation.response.clientDataJSON)
-        .reduce((s, byte) => s + String.fromCharCode(byte), ''));
-      response.attestationObject = btoa(
-        new Uint8Array(attestation.response.attestationObject)
-        .reduce((s, byte) => s + String.fromCharCode(byte), ''));
-      publicKeyCredential.response = response;
-
-      // Send new credential back to Relying Party for validation and storage
-      finishAddCredential(publicKeyCredential, options.session.id);
-    } else {
-      addErrorMsg("Make Credential response lacking 'response' attribute");
-    }
-  }).catch(function (err) {
-    hide('#active');
-    console.log(err.toString());
-    addErrorMsg(`An error occurred during Make Credential operation [${err.toString()}]`);
-  });
-}
-
-function finishAssertion(publicKeyCredential, sessionId) {
-  post('/FinishGetAssertion', {
-    data: JSON.stringify(publicKeyCredential),
-    session: sessionId }
-  ).then(parameters => {
-    console.log(parameters);
-    if ('success' in parameters && 'message' in parameters) {
-      addSuccessMsg(parameters.message);
-      if ('handle' in parameters) {
-        highlightCredential(parameters.handle);
+    document.querySelector('#auth').addEventListener('click', () => {
+      this.getAssertion();
+    });
+    document.querySelector('#credentials').addEventListener('click', e => {
+      e.stopPropagation();
+      if (e.target.nodeName == 'PAPER-BUTTON') {
+        this.delete(e.target.id);
       }
-    }
-    // TODO Validate response and display success/error message
-  });
-}
-
-function highlightCredential(handle) {
-  $(`#${handle}`).effect("highlight", {color: '#FF4081'}, 7500);
-}
-
-function addErrorMsg(msg) {
-  $('#error-text').innerHTML = msg;
-  show('#error');
-}
-
-function addSuccessMsg(msg) {
-  $('#success-text').innerHTML = msg;
-  show('#success');
-}
-
-function removeMsgs() {
-  hide('#error');
-  hide('#success');
-}
-
-function getAssertion() {
-  removeMsgs();
-  show('#active');
-  post('/BeginGetAssertion').then(parameters => {
-    let requestOptions = {};
-    requestOptions.challenge = Uint8Array.from(atob(parameters.challenge), c => c.charCodeAt(0));
-    if ('timeout' in parameters) {
-      requestOptions.timeout = parameters.timeout;
-    }
-    if ('rpId' in parameters) {
-      requestOptions.rpId = parameters.rpId;
-    }
-    if ('allowCredentials' in parameters) {
-      requestOptions.allowCredentials = credentialListConversion(parameters.allowCredentials);
-    }
-
-    let credentialRequest = {};
-    credentialRequest.publicKey = requestOptions;
-    console.log(credentialRequest);
-
-    if (typeof navigator.credentials.get !== "function") {
-      throw "Browser does not support credential lookup";
-    }
-
-    return navigator.credentials.get({
-      "publicKey": requestOptions
     });
-  }).then(assertion => {
-    hide('#active');
-    let publicKeyCredential = {};
-    if ('id' in assertion) {
-      publicKeyCredential.id = assertion.id;
+
+    this.fetchCredentials();
+  }
+
+  _fetch(url, obj) {
+    let headers = new Headers({
+      'Content-Type': 'application/x-www-form-urlencoded'
+    });
+    let body = new URLSearchParams();
+    for (let key in obj) {
+      body.append(key, obj[key]);
     }
-    if ('type' in assertion) {
-      publicKeyCredential.type = assertion.type;
+    return fetch(url, {
+      method: 'POST',
+      headers: headers,
+      credentials: 'include',
+      body: body
+    }).then(response => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw response.statusText;
+      }
+    });
+  }
+
+  fetchCredentials() {
+console.log('[/RegisteredKeys] request');
+
+    this._fetch('/RegisteredKeys').then(response => {
+      this.app.credentials = response;
+    });
+  }
+
+  addCredential() {
+    this.app.active = true;
+
+    const advancedOptions = {};
+    let options;
+
+    if (this.app.advanced) {
+      advancedOptions.requireResidentKey = this.app.requireResidentKey;
+      advancedOptions.excludeCredentials = this.app.excludeCredentials;
+      advancedOptions.userVerification = this.app.userVerification;
+      advancedOptions.authenticatorAttachment = this.app.authenticatorAttachment;
+      advancedOptions.attestationConveyancePreference = this.app.attestationConveyancePreference;
     }
-    if ('rawId' in assertion) {
-      publicKeyCredential.rawId = btoa(
-        new Uint8Array(assertion.rawId).reduce((s, byte) =>
-        s + String.fromCharCode(byte), ''));
-      publicKeyCredential.rawId = assertion.rawId;
-    }
-    if ('response' in assertion) {
-      var response = {};
-      response.clientDataJSON = btoa(
-        new Uint8Array(assertion.response.clientDataJSON)
-        .reduce((s, byte) => s + String.fromCharCode(byte), ''));
-      response.authenticatorData = btoa(
-        new Uint8Array(assertion.response.authenticatorData)
-        .reduce((s, byte) => s + String.fromCharCode(byte), ''));
-      response.signature = btoa(
-        new Uint8Array(assertion.response.signature)
-        .reduce((s, byte) => s + String.fromCharCode(byte), ''));
-      response.userHandle = btoa(
-        new Uint8Array(assertion.response.userHandle)
-        .reduce((s, byte) => s + String.fromCharCode(byte), ''));
+
+console.log('[/BeginMakeCredential] request options:', advancedOptions);
+
+    this._fetch('/BeginMakeCredential', {
+      advanced: this.app.advanced,
+      advancedOptions: JSON.stringify(advancedOptions)
+    }).then(_options => {
+      options = _options;
+console.log('[/BeginMakeCredential] response:', options);
+
+      const makeCredentialOptions = {};
+
+      makeCredentialOptions.rp = options.rp;
+      makeCredentialOptions.user = options.user;
+      makeCredentialOptions.user.id = new TextEncoder().encode(options.user.id);
+      makeCredentialOptions.challenge = this._strToBin(options.challenge);
+      makeCredentialOptions.pubKeyCredParams = options.pubKeyCredParams;
+
+      // Optional parameters
+      if ('timeout' in options) {
+        makeCredentialOptions.timeout = options.timeout;
+      }
+      if ('excludeCredentials' in options) {
+        makeCredentialOptions.excludeCredentials = this._credentialListConversion(options.excludeCredentials);
+      }
+      if ('authenticatorSelection' in options) {
+        makeCredentialOptions.authenticatorSelection = options.authenticatorSelection;
+      }
+      if ('attestation' in options) {
+        makeCredentialOptions.attestation = options.attestation;
+      }
+      if ('extensions' in options) {
+        makeCredentialOptions.extensions = options.extensions;
+      }
+
+console.log('`navigator.credentials.create()` options:', makeCredentialOptions);
+
+      // Check to see if the browser supports credential creation
+      if (typeof navigator.credentials.create !== "function") {
+        throw "Browser does not support credential creation";
+      }
+
+      return navigator.credentials.create({
+        publicKey: makeCredentialOptions
+      });
+    }).then(attestation => {
+      this.app.active = false;
+
+console.log('`navigator.credentials.create()`result:', attestation);
+
+      const publicKeyCredential = {};
+
+      if ('id' in attestation) {
+        publicKeyCredential.id = attestation.id;
+      }
+      if ('type' in attestation) {
+        publicKeyCredential.type = attestation.type;
+      }
+      if ('rawId' in attestation) {
+        publicKeyCredential.rawId = this._binToStr(attestation.rawId);
+      }
+      if (!attestation.response) {
+        throw "Make Credential response lacking 'response' attribute";
+      }
+
+      const response = {};
+      response.clientDataJSON = this._binToStr(attestation.response.clientDataJSON);
+      response.attestationObject = this._binToStr(attestation.response.attestationObject);
       publicKeyCredential.response = response;
-      finishAssertion(publicKeyCredential, parameters.session.id);
-    }
-  }).catch(function (err) {
-    hide('#active');
-    console.log(err.toString());
-    addErrorMsg(`An error occurred during Assertion request [${err.toString()}]`);
-  });
+
+console.log('[/FinishMakeCredential] request options:', publicKeyCredential);
+
+      return this._fetch('/FinishMakeCredential', {
+        data: JSON.stringify(publicKeyCredential),
+        session: options.session.id
+      });
+    }).then(parameters => {
+console.log('[/FinishMakeCredential] response:', parameters);
+
+      if (parameters && parameters.success) {
+        this._showMessage(parameters.message);
+        this.fetchCredentials();
+      } else {
+        throw 'Unexpected response received.';
+      }
+    }).catch(err => {
+      this.app.active = false;
+
+      this._showMessage(`An error occurred during Make Credential operation [${err.toString()}]`);
+    });
+  }
+
+  getAssertion() {
+    this.app.active = true;
+    let parameters;
+
+console.log('[/BeingGetAssertion] request');
+
+    this._fetch('/BeginGetAssertion').then(_parameters => {
+      parameters = _parameters;
+      const requestOptions = {};
+
+      requestOptions.challenge = this._strToBin(parameters.challenge);
+      if ('timeout' in parameters) {
+        requestOptions.timeout = parameters.timeout;
+      }
+      if ('rpId' in parameters) {
+        requestOptions.rpId = parameters.rpId;
+      }
+      if ('allowCredentials' in parameters) {
+        requestOptions.allowCredentials = this._credentialListConversion(parameters.allowCredentials);
+      }
+
+console.log('`navigator.credentials.get()` result:', requestOptions);
+
+      if (typeof navigator.credentials.get !== "function") {
+        throw "Browser does not support credential lookup";
+      }
+
+      return navigator.credentials.get({
+        publicKey: requestOptions
+      });
+    }).then(assertion => {
+      this.app.active = false;
+
+console.log('`navigator.credentials.get()` result:', assertion);
+
+      const publicKeyCredential = {};
+
+      if ('id' in assertion) {
+        publicKeyCredential.id = assertion.id;
+      }
+      if ('type' in assertion) {
+        publicKeyCredential.type = assertion.type;
+      }
+      if ('rawId' in assertion) {
+        publicKeyCredential.rawId = this._binToStr(assertion.rawId);
+      }
+      if (!assertion.response) {
+        throw "Get assertion response lacking 'response' attribute";
+      }
+
+      const response = {};
+
+      response.clientDataJSON = this._binToStr(assertion.response.clientDataJSON);
+      response.authenticatorData = this._binToStr(assertion.response.authenticatorData);
+      response.signature = this._binToStr(assertion.response.signature);
+      response.userHandle = this._binToStr(assertion.response.userHandle);
+      publicKeyCredential.response = response;
+
+console.log('[/FinishGetAssertion] request options:', publicKeyCredential);
+      return this._fetch('/FinishGetAssertion', {
+        data: JSON.stringify(publicKeyCredential),
+        session: parameters.session.id
+      });
+    }).then(parameters => {
+console.log('[/FinishGetAssertion] response:', parameters);
+
+      if (parameters && parameters.success) {
+        this._showMessage(parameters.message);
+        if ('handle' in parameters) {
+          document.querySelector(`#${parameters.handle}`).style =
+            'background-color: fuchsia';
+        }
+      } else {
+        throw 'Unexpected response received.';
+      }
+    }).catch(err => {
+      this.app.active = false;
+
+      this._showMessage(`An error occurred during Assertion request [${err.toString()}]`);
+    });
+  }
+
+  _credentialListConversion(list) {
+    return list.map(item => {
+      return {
+        type: item.type,
+        id: this._strToBin(item.id),
+        transports: list.transports || undefined
+      };
+    });
+  }
+
+  _finishAssertion(publicKeyCredential, sessionId) {
+  }
+
+  _strToBin(str) {
+    return Uint8Array.from(atob(str), c => c.charCodeAt(0));
+  }
+
+  _binToStr(bin) {
+    return btoa(new Uint8Array(bin).reduce(
+      (s, byte) => s + String.fromCharCode(byte), ''
+    ));
+  }
+
+  _showMessage(text) {
+    this.toast.text = text;
+    this.toast.show();
+  }
+
+  delete(id) {
+console.log('[/RemoveCredential] id:', id);
+
+    this._fetch('/RemoveCredential', {
+      credentialId : id
+    }).then(() => {
+      this.fetchCredentials();
+    });
+  }
 }
+
+const app = new WadApp();
