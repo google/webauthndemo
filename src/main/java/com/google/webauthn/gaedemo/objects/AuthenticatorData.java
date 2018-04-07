44c5a6df7e5ef37a4636cf104b6c896779acdf14
kasecato <kasecato@gmail.com>
2018-04-07 20:20:34 +0900

    Fix an auth flag where both operands are the same is usually incorrect

--- src/main/java/com/google/webauthn/gaedemo/objects/AuthenticatorData.java
+++ src/main/java/com/google/webauthn/gaedemo/objects/AuthenticatorData.java
@@ -193,7 +193,7 @@ public class AuthenticatorData {
     try {
       if (obj instanceof AuthenticatorData) {
         AuthenticatorData other = (AuthenticatorData) obj;
-        if (other.flags == other.flags) {
+        if (flags == other.flags) {
           if (Arrays.equals(other.rpIdHash, rpIdHash)) {
             if (signCount == other.signCount) {
               if (attData == null && other.attData == null) {
